5171c0c5b1ae607f421372aec933577000b6e8f9
law-al <lawahm303@gmail.com>
2025-09-27 21:38:55 +0100

    fix: fix cors issues v_2

--- api/src/secret.ts
+++ api/src/secret.ts
@@ -4,6 +4,7 @@ const envFile =
 dotenv.config({ path: envFile });
 
 export const PORT = process.env.PORT || 5000;
+export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const NODE_ENV = process.env.NODE_ENV;
 export const EMAIL_USER = process.env.EMAIL_USER;
