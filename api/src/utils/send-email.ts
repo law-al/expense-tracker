65ab1e73668ea4f8fdd977bf3d4228fcac05bec3
law-al <lawahm303@gmail.com>
2025-09-29 14:16:08 +0100

    fix: fix email sending

--- api/src/utils/send-email.ts
+++ api/src/utils/send-email.ts
@@ -2,15 +2,14 @@ import nodemailer from 'nodemailer';
 import { EMAIL_USER, EMAIL_PASSWORD } from '../secret.js';
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
+  // --- RECOMMENDED CONFIGURATION ---
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: EMAIL_USER,
       pass: EMAIL_PASSWORD,
-    },
-
-    tls: {
-      rejectUnauthorized: false,
     },
   });
 
